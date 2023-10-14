4a070218ee64488ee51071aaae2ffcc1ac6b44eb
rickyurvinaUCN <ricardo.urvina@alumnos.ucn.cl>
2023-10-14 15:15:42 -0300

    Navigate to profile with login

--- app/src/main/java/com/example/loginnavigate/ProfileActivity.java
+++ app/src/main/java/com/example/loginnavigate/ProfileActivity.java
@@ -2,13 +2,27 @@ package com.example.loginnavigate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView txt_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        String email= getIntent().getStringExtra("i_email");
+        txt_email = findViewById(R.id.txt_email);
+        txt_email.setText(email);
+        Toast.makeText(this, "Welcome "+email, Toast.LENGTH_SHORT).show();
+    }
+
+    public void navigate(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/loginnavigate/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/loginnavigate/ui/home/HomeFragment.java
@@ -1,25 +1,39 @@
 package com.example.loginnavigate.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.loginnavigate.ProfileActivity;
 import com.example.loginnavigate.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    private EditText txt_email, txt_password;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        txt_email = binding.txtEmail;
+        txt_password = binding.txtPassword;
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
 
         return root;
     }
@@ -28,5 +42,25 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void login() {
+        String email = txt_email.getText().toString();
+        String password = txt_password.getText().toString();
+        if (email.equals("admin@admin.com") && password.equals("admin")) {
+            Toast.makeText(this.getContext(), "Login Success", Toast.LENGTH_SHORT).show();
+            navigateToProfile();
+        } else {
+            Toast.makeText(this.getContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+        }
+//        Intent next = new Intent(this.getContext(), MainActivity2.class);
+//        next.putExtra("info", txt_info.getText().toString());
+//        startActivity(next);
+    }
+
+    public void navigateToProfile() {
+        Intent next = new Intent(this.getContext(), ProfileActivity.class);
+        next.putExtra("i_email", txt_email.getText().toString());
+        startActivity(next);
     }
 }
